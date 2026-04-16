71185d964057c644d546a31da332b6e7c350ba34
SaranHiruthikM <saran.hiruthik83@gmail.com>
2026-04-16 13:44:02 +0530

    feat: implement token bucket rate limiting for connection handling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 	"sync"
+	"time"
 )
 
 type Job struct {
@@ -14,6 +15,42 @@ type WorkerPool struct {
 	jobs       chan Job
 	maxWorkers int
 	wg         sync.WaitGroup
+}
+
+type TokenBucket struct {
+	MaxTokens  int64
+	Tokens     int64
+	Rate       int64
+	LastRefill time.Time
+	Mutex      sync.Mutex
+}
+
+func NewTokenBucket(rate, maxTokens int64) *TokenBucket {
+	newBucket := &TokenBucket{
+		MaxTokens:  maxTokens,
+		Tokens:     maxTokens,
+		Rate:       rate,
+		LastRefill: time.Now(),
+	}
+
+	return newBucket
+}
+
+func (tb *TokenBucket) Allow() bool {
+	tb.Mutex.Lock()
+	defer tb.Mutex.Unlock()
+
+	timeElapsed := time.Since(tb.LastRefill).Seconds()
+	tokensToAdd := timeElapsed * float64(tb.Rate)
+	tb.Tokens = min(tb.Tokens+int64(tokensToAdd), tb.MaxTokens)
+	tb.LastRefill = time.Now()
+
+	if tb.Tokens > 0 {
+		tb.Tokens--
+		return true
+	}
+
+	return false
 }
 
 func NewWorkerPool(maxWorkers, queueSize int) *WorkerPool {
@@ -61,6 +98,7 @@ func (w *WorkerPool) Close() {
 
 func main() {
 	workers := NewWorkerPool(1, 10)
+	rateLimiter := NewTokenBucket(2, 5)
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal("Listener failed..")
@@ -70,6 +108,12 @@ func main() {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Println("Client not connected")
+			continue
+		}
+
+		if !rateLimiter.Allow() {
+			conn.Write([]byte("HTTP/1.1 429 Too Many Requests\r\n\r\nRate limit exceeded\n"))
+			conn.Close()
 			continue
 		}
 
